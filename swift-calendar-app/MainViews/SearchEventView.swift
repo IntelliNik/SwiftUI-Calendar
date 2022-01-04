110528b2e44605d1e7010db3bc685cc4d1970e6f
DR <d.rademacher@gmx.net>
2022-01-04 14:04:30 +0000

    Merged in feature/SCA-46-provide-a-search (pull request #20)
    Add search of events by name.
    
    * Add search of events by name.
    
    * merge with main.
    
    
    Approved-by: Niklas Schulte

--- swift-calendar-app/MainViews/SearchEventView.swift
+++ swift-calendar-app/MainViews/SearchEventView.swift
@@ -7,17 +7,42 @@
 
 import SwiftUI
 
-struct SearchEventView: View {
-    @State var searchBarText: String = ""
-    
+struct SearchEventView: View {    
+    @State private var query = ""
+
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.name, order: .forward)])
+    private var events: FetchedResults<Event>
+           
     var body: some View {
-        NavigationView{
-            TextField("Search for events ...", text: $searchBarText).padding()
-                .navigationTitle("Search for events")
-            // TODO: display some events here, sorted by today
+        NavigationView {
+            if query.isEmpty{
+                Text("Searching for...")
+                    .listStyle(.plain)
+                    .navigationTitle("Search Events")
+            } else {
+                List(events) { event in
+                  /*NavigationLink(destination:
+                    EventView(event: event))
+                   */
+                    Text("Name: \(event.name ?? "") in Calendar: \(event.calendar?.name ?? "No Calendar")")
+                }
+                .listStyle(.plain)
+                .navigationTitle("Search Events")
+            }
         }
+        .searchable(text: $query, placement: .navigationBarDrawer(displayMode: .automatic), prompt: "Search events")
+        .onChange(of: query) { newValue in
+            events.nsPredicate = searchPredicate(query: newValue)
+         }
+        
+    }
+        
+    private func searchPredicate(query: String) -> NSPredicate? {
+        if query.isEmpty { return nil }
+        return NSPredicate(format: "name BEGINSWITH %@", query)
     }
 }
+
 
 struct SearchEventView_Previews: PreviewProvider {
     static var previews: some View {
