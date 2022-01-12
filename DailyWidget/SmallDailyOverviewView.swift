43b536a64a64398666631fad1aeebcc35e5de16e
Niklas Schulte <niklas.schulte@senacor.com>
2022-01-12 21:05:33 +0100

    first widget

--- DailyWidget/SmallDailyOverviewView.swift
+++ DailyWidget/SmallDailyOverviewView.swift
@@ -16,7 +16,42 @@ struct SmallDailyOverviewView: View {
     ) var events: FetchedResults<Event>
     
     var body: some View {
-        Text(events[0].name ?? "AAAA")
+        HStack{
+            Spacer()
+            Text(Date.now, formatter: getDayFormatter()).padding([.top, .trailing])
+        }
+        HStack{
+            Spacer()
+            Text(getWeekday()).padding(.trailing)
+        }
+        Spacer()
+        if(events.count == 0){
+            Text("You have no events today.").padding()
+        }else if(events.count == 1){
+            Text("You have 1 event today.").padding()
+        } else if (events.count > 1){
+            Text("You have \(events.count) events today.").padding()
+        }
+    }
+}
+
+func getDayFormatter() -> DateFormatter{
+    let formatter = DateFormatter()
+    formatter.dateFormat = "dd"
+    return formatter
+}
+
+func getWeekday() -> String{
+    let components = Calendar.current.dateComponents([.weekday], from: Date.now)
+    switch components.weekday {
+    case 1: return "Sunday"
+    case 2: return "Monday"
+    case 3: return "Tuesday"
+    case 4: return "Wednesday"
+    case 5: return "Thursday"
+    case 6: return "Friday"
+    case 7: return "Saturday"
+    default: return ""
     }
 }
 
