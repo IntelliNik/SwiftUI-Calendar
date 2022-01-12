288d0702fea3e650a924ba0ebdaf0efd04feee4c
Niklas Schulte <niklas.schulte@senacor.com>
2022-01-12 17:37:44 +0100

    work on widget

---
+++ CalendarWidget/SmallDailyOverviewView.swift
@@ -0,0 +1,30 @@
+//
+//  DailyOverviewView.swift
+//  swift-calendar-app
+//
+//  Created by Schulte, Niklas on 10.01.22.
+//
+
+import SwiftUI
+
+struct SmallDailyOverviewView: View {
+    /*
+    @FetchRequest(
+        entity: Event.entity(),
+        sortDescriptors: [
+            NSSortDescriptor(keyPath: \Event.startdate, ascending: true),
+        ]
+    ) var events: FetchedResults<Event>
+     */
+    
+    var body: some View {
+        //Text(events[0].name ?? "")
+        Text("")
+    }
+}
+
+struct SmallDailyOverviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        SmallDailyOverviewView()
+    }
+}
