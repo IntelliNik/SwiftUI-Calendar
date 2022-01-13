f6ceb2069c27e45469638c1ac898237389b29bfd
Niklas Schulte <niklas.schulte@rwth-aachen.de>
2022-01-13 20:09:53 +0000

    Merged in get-started-with-widget (pull request #35)
    Implement the Small, Medium and Large Widget
    
    * first try with widget
    
    * work on widget
    
    * Allow to use CoreDate for Widget
    
    * first widget
    
    * SCA-51 implement MediumWidget
    
    * Merge branch 'main' into feature/SCA-4-week-view
    
    * SCA-51 more changes after merge
    
    * SCA-51 fix some stuff after merge
    
    * SCA-51 implement large widget
    
    * SCA-51 comment out widgets that crash
    
    
    Approved-by: Andreas Dymek

---
+++ DailyWidget/DailyWidget.swift
@@ -0,0 +1,86 @@
+//
+//  DailyWidget.swift
+//  DailyWidget
+//
+//  Created by Schulte, Niklas on 08.01.22.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct DailyWidgetEntryView : View {
+    var entry: Provider.Entry
+    
+    @Environment(\.widgetFamily) var widgetFamily
+    
+    @StateObject private var dataController = DataController()
+
+    var body: some View {
+        if(widgetFamily == .systemSmall){
+            SmallDailyOverviewView(dateComponents: Calendar.current.dateComponents([.day], from: Date.now))
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+        } else if (widgetFamily == .systemMedium){
+            // TODO fetch here is not working properly, app crashes
+            //MediumDailyOverviewView()
+                //.environment(\.managedObjectContext, dataController.container.viewContext)
+        } else{
+            // TODO fetch here is not working properly, app crashes
+            //LargeDailyOverviewView()
+                //.environment(\.managedObjectContext, dataController.container.viewContext)
+        }
+    }
+}
+
+@main
+struct CalendarWidget: Widget {
+    let kind: String = "CalendarWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            DailyWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Daily Overview")
+        .description("This widget keeps you updated with your events today.")
+    }
+}
+
+struct CalendarWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        DailyWidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+        DailyWidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
+        DailyWidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemLarge))
+    }
+}
