d80d9ff51faca622485e3259bc14f576c6f962f0
Farhadiba Mohammed <farhadiba.mohammed@rwth-aachen.de>
2022-01-18 13:09:12 +0000

    Merged in day-view-merge (pull request #48)
    included day view files in current version
    
    * included day view files in current version
    
    * few fixes and design changes
    
    * Repair stuff that went wrong for no good reason other than Xcode is weird
    
    
    Approved-by: Lucas Wollenhaupt

---
+++ swift-calendar-app/MainViews/DayView/ViewHelpers/DayViewTime.swift
@@ -0,0 +1,86 @@
+//
+//  DayViewTime.swift
+//  swift-calendar-app
+//
+//  Created by Farhadiba Mohammed on 08.01.22.
+//
+
+import SwiftUI
+struct DayViewTime: View {
+    @Binding var dateComponents: DateComponents
+    
+    @State var eventsToday: FetchedResults<Event>
+    
+    @State var showEventSheet = false
+    @State var eventToShow: Event?
+    
+    func filterEventsForHour(hour: Int) -> [Event]{
+        var foundEvents: [Event] = []
+        eventsToday.forEach{ event in
+            if(Calendar.current.component(.hour, from: event.startdate!) == hour){
+                foundEvents.append(event)
+            }
+        }
+        return foundEvents
+    }
+    
+    var body: some View {
+        let cur_hour = getToday().hour
+        let cur_day = getToday().day
+        ScrollViewReader{ scroll in
+            ScrollView{
+                ZStack{
+                    VStack(alignment: .leading, spacing: 25){
+                        ForEach(0...23, id:\.self){ hour in
+                            GeometryReader{ geometry in
+                                HStack{
+                                    ZStack{
+                                        Text("\(String(hour)):00")
+                                            .padding([.top, .bottom]).frame(width: geometry.size.width * 0.2)
+                                        if (cur_day == dateComponents.day){
+                                            if (cur_hour == hour){
+                                                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                                    .stroke(Color(getAccentColorString()), lineWidth: 2.0)
+                                                    .frame(width: 50, height: 45)
+                                            }
+                                        }
+                                    }
+                                    ZStack{
+                                        VStack(alignment: .leading){
+                                            let eventsThisHour: [Event] = filterEventsForHour(hour: hour)
+                                            ScrollView(.horizontal){
+                                                HStack(){
+                                                    ForEach(eventsThisHour, id:\.self){ event in
+                                                        EventView(event: event).onTapGesture(){
+                                                            eventToShow = event
+                                                            showEventSheet = true
+                                                        }
+                                                    }
+   
+                                                }
+                                            }.padding(.trailing, 30)
+                                        }.zIndex(1)
+                                        
+                                        Rectangle().fill(Color(UIColor.lightGray)).frame(height: 2).padding(.trailing, 30)
+                                    }
+                                }
+                            }
+                            Spacer().frame(height: 20)
+                        }
+                        Spacer()
+                    }
+                }.sheet(isPresented: $showEventSheet){
+                    ShowEventView(event: eventToShow!)
+                }
+            }
+        }
+    }
+}
+
+
+//struct DayViewTime_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DayViewTime(dateComponents: .constant(Calendar.current.dateComponents([.weekday, .day, .month, .year], from: Date.now)))
+//    }
+//}
+
